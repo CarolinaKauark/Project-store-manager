13a9a84611189d926d173341540365b4e8990776
Ckauark <ckauark@gmail.com>
2022-09-12 17:32:31 -0300

    feat: req 10

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -27,8 +27,19 @@ const addProduct = async (req, res) => {
   res.status(201).json(message);
 };
 
+const updateProductById = async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const { type, message } = await productsService.changeProductById(id, name);
+
+  if (type) return res.status(type).json({ message });
+
+  res.status(200).json(message);
+};
+
 module.exports = {
   listProducts,
   listProductsById,
   addProduct,
+  updateProductById,
 };
